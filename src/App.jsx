0448f8637553fab7a1e8ed095f8be1aef5c23774
Kanha-Codes-XD <85252018+Kanha02052002@users.noreply.github.com>
2024-09-15 17:57:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <CurrencyConverter />
       <footer className="footer">
         <p>Developed by <a href="https://github.com/Kanha02052002" target="_blank" rel="noopener noreferrer">Kanha Khantaal </a>
-|| For source code visit this <a href="https://github.com/Kanha02052002/currencyconverter" target="_blank" rel="noopener noreferrer">Repo</a></p>
+|| For source code visit this <a href="https://github.com/Kanha02052002/currencyConverter_1" target="_blank" rel="noopener noreferrer">Repo</a></p>
       </footer>
     </div>
   );
